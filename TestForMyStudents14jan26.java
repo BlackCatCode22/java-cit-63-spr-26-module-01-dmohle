e4a214c8c56a10fd36592ebf115f5d799cf258eb
dmohle <10038913+dmohle@users.noreply.github.com>
2026-01-14 18:33:18 -0800

    Add files via upload

---
+++ TestForMyStudents14jan26.java
@@ -0,0 +1,11 @@
+// TestForStudents14jan26.java
+// dH 1/14/26
+
+public class TestForMyStudents14jan26 {
+    public static void main(String[] args) {
+
+        System.out.println("\n\nWelcome to My java test program\n\n");
+
+ 
+    }
+}
